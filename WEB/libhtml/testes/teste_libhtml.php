f007416ad99dc80c4080d729cd8c2b680257ed97
JoaoVitorGir <joao.girardi@unidavi.edu.br>
2023-03-29 20:41:34 -0300

    atualizações

--- WEB/libhtml/testes/teste_libhtml.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "classes/a.class.php";
-
-echo(new A("http://google.com","google","btn","_blank","Google"));
-echo(new A("http://microsoft.com","ms","btn","_blank","Microsoft"));
